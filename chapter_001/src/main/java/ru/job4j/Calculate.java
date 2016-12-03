f38c04d4938215f1d6803ef215de2ab73f477484
YuzyakAV <YuzyakAV@gmail.com>
2016-12-03 17:53:48 +0300

    add Calculate.java

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,16 @@
 package ru.job4j;
 
+/**
+* Calculate Класс для вывода текста на экран.
+* @author ayuzyak
+* @version 1
+* @since 03.12.2016
+ */
  public class Calculate {
+	 /**
+     * Main. 
+     * @param args - args.
+     */
 public static void main(String[] args){
 System.out.println("Hello world.");
 }
