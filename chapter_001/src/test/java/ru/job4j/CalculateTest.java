39d958c33c4aba865bbacf4f31b68854dda1975e
YuzyakAV <YuzyakAV@gmail.com>
2016-12-05 21:27:10 +0300

    CalculateTest

--- chapter_001/src/test/java/ru/job4j/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -5,13 +5,13 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
-
 /**
 * Test.
 * @author ayuzyak.
 * @version 1.
 * @since 04.12.2016.
 */
+
 public class CalculateTest {
 /**
 * Test add.
