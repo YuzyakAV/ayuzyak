eef762571a9fb98079c9d6ec8e4a552320573b88
YuzyakAV <YuzyakAV@gmail.com>
2016-12-13 23:46:00 +0300

    Add files via upload

---
+++ chapter_001/src/main/java/ru/job4j/Point.java
@@ -0,0 +1,36 @@
+package ru.job4j;
+
+/**
+ * Vector point class.
+ * @author ayuzyak.
+ * @version 1.
+ * @since 12.12.2016.
+*/
+public class Point {
+    /**
+     * Point x.
+     */
+    public double x;
+    /**
+     * Point y.
+     */
+    public double y;
+
+    /**
+     * Point constructor.
+     * @param x - x axis coordinate
+     * @param y - y axis coordinate
+     */
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * Distance to the point.
+     * @param point - provided point
+     * @return distance
+     */
+    public double distanceTo(Point point) {
+        return Math.sqrt(Math.pow(point.x - this.x, 2) + Math.pow(point.y - this.y, 2));
+    }
