ad1d012302e51223e12fd808160cc993282a705e
YuzyakAV <YuzyakAV@gmail.com>
2016-12-19 20:54:14 +0300

    add Counter.java

---
+++ chapter_001/src/main/java/ru/job4j/Counter.java
@@ -0,0 +1,28 @@
+package ru.job4j;
+
+/**
+ * Counter class.
+ * @author ayuzyak.
+ * @version 1.
+ * @since 18.12.2016.
+ */
+public class Counter {
+
+    /**
+     * Calculate even numbers.
+     * @param start - from
+     * @param finish - to
+     * @return
+     */
+    public int add(int start, int finish) {
+        int summ = 0;
+        for (int i = start; i <= finish; i++) {
+            if (i % 2 != 0) {
+                continue;
+            }
+            summ += i;
+        }
+
+        return  summ;
+    }
+}
