c50d849ca5fd8875ad688974f51bac63417f1501
YuzyakAV <YuzyakAV@gmail.com>
2017-02-05 12:40:58 +0300

    Add files via upload

---
+++ chapter_001/src/test/java/ru/job4j/StringOperationTest.java
@@ -0,0 +1,34 @@
+package ru.job4j;
+
+import org.junit.Test;
+import ru.job4j.StringOperations;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * StringOperations test.
+ * @author ayuzyak.
+ * @version 1.
+ * @since 10.01.2017.
+ */
+public class StringOperationsTest {
+
+    /**
+     * Check if origin string contains substring.
+     */
+    @Test
+    public void whenStringContainsSubstring() {
+        StringOperations operations = new StringOperations();
+        assertThat(operations.contains("Vladimir Putin","Vladimir"), is(true));
+    }
+
+    /**
+     * Check if origin string not contains substring.
+     */
+    @Test
+    public void whenStringNotContainsSubstring() {
+        StringOperations operations = new StringOperations();
+        assertThat(operations.contains("Dmitry Medvedev","Vladimir"), is(false));
+    }
+}
