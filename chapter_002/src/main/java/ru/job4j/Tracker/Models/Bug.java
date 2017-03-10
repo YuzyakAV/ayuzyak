1913c81a3311695b414bf7085d41fc4b0b3aa635
YuzyakAV <YuzyakAV@gmail.com>
2017-03-10 23:19:00 +0300

    C

---
+++ chapter_002/src/main/java/ru/job4j/Tracker/Models/Bug.java
@@ -0,0 +1,17 @@
+package ru.job4j.tracker.models;
+
+/**
+ * Class Bug.
+ * @author yuzyakav
+ * @since 06.03.2017
+ * @version 1.0
+ */
+
+public class Bug extends Item {
+	/**
+	 * Default Constructor Bug.
+	 */
+	public Bug() {
+		super();
+	}
+}
