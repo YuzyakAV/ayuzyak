5477539d609e0fd8125a31e96902a976c156a8f4
YuzyakAV <YuzyakAV@gmail.com>
2017-03-10 23:11:08 +0300

    C

---
+++ chapter_002/src/main/java/ru/job4j/Tracker/Start/Enter.java
@@ -0,0 +1,32 @@
+package ru.job4j.tracker.start;
+
+/**
+ * Class Enter.
+ * @author yuzyakav
+ * @since 04.03.2017
+ * @version 1.0
+ */
+public class Enter {
+
+    /**
+     * input for data.
+     */
+    private Input input;
+
+    /**
+     * Constructor with input.
+     * @param input - tracker for edit.
+     */
+    public Enter(Input input) {
+        this.input = input;
+    }
+
+    /**
+     * execute enter.
+     * @param question - asking information.
+     * @return String.
+     */
+    public String execute(String question) {
+        return input.ask(question);
+    }
+}
