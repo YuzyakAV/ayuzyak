335a1797d5bd323c12b0285f5f00231a218b3c4b
YuzyakAV <YuzyakAV@gmail.com>
2017-03-10 23:10:43 +0300

    C

---
+++ chapter_002/src/main/java/ru/job4j/Tracker/Start/BaseAction.java
@@ -0,0 +1,46 @@
+package ru.job4j.tracker.start;
+
+/**
+ * Class BaseAction.
+ * @author yuzyak
+ * @since 07.03.2017
+ * @version 1.0
+ */
+public abstract class BaseAction implements UserAction {
+    /**
+     * name.
+     */
+    private String name;
+
+    /**
+     * constructor for BaseAction.
+     * @param name of object's.
+     */
+    public BaseAction(String name) {
+        this.name = name;
+    }
+
+    /**
+     * key for actions.
+     * @return int key.
+     */
+    @Override
+    public abstract int key();
+
+    /**
+     * method for execute action.
+     * @param input for enter information.
+     * @param tracker of tasks.
+     */
+    @Override
+    public abstract void execute(Input input, Tracker tracker);
+
+    /**
+     * method for print information.
+     * @return String information.
+     */
+    @Override
+    public String info() {
+        return String.format("%s. %s", this.key(), name);
+    }
+}
