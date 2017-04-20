e0a0deb321a88e5e8d46f1c3ad65eaccb598ef1a
YuzyakAV <YuzyakAV@gmail.com>
2017-04-20 19:45:50 +0300

    Update ValidateInput.java

--- chapter_002/src/main/java/ru/job4j/tracker/start/ValidateInput.java
+++ chapter_002/src/main/java/ru/job4j/tracker/start/ValidateInput.java
@@ -1,16 +1,18 @@
 package ru.job4j.tracker.start;
 
+import java.util.ArrayList;
+
 /**
  * Class ValidateInput.
- * @author yuzyakav.
- * @since 10.03.2017
+ * @author Ayuzyak
+ * @since 19.03.2017
  * @version 1.0
  */
 
 public class ValidateInput extends ConsoleInput {
 
     @Override
-    public int ask(String question, int[] range) {
+    public int ask(String question, ArrayList<Integer> range) {
         boolean invalid = true;
         int value = -1;
         do {
