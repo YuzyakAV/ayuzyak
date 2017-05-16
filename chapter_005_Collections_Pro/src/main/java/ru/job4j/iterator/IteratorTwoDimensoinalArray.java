deb99eee7d2c78978730fde9fff8b3ad33fce41a
YuzyakAV <yuzyakav@gmail.com>
2017-05-16 22:20:41 +0300

    C

---
+++ chapter_005_Collections_Pro/src/main/java/ru/job4j/iterator/IteratorTwoDimensoinalArray.java
@@ -0,0 +1,65 @@
+package ru.job4j.iterator;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Iterator for two-dimensional array.
+ *
+ * @author Ayuzyak
+ * @since 13.05.2017
+ * @version 1.0
+ * @param <E> type of elements.
+ */
+public class IteratorTwoDimensoinalArray<E> implements Iterator<E> {
+    /**
+     * Two dimensional array.
+     */
+    private final E[][] array;
+
+    /**
+     * Cursor for passage subarray elements.
+     */
+    private int cursorX = 0;
+
+    /**
+     * Cursor for passage arrays.
+     */
+    private int cursorY = 0;
+
+    /**
+     * Constructor for Iterator.
+     * @param array for constructing Iterator.
+     */
+    public IteratorTwoDimensoinalArray(final E[][] array) {
+        this.array = array;
+    }
+
+    /**
+     * Check next element.
+     * @return true if iterator has next element.
+     */
+    @Override
+    public boolean hasNext() {
+        return cursorY < array.length;
+    }
+
+    /**
+     * Get next element.
+     * @return next element.
+     */
+    @Override
+    public E next() {
+        E element;
+        if (cursorY < array.length) {
+            element = array[cursorY][cursorX++];
+        } else {
+            throw new NoSuchElementException();
+        }
+        if (cursorX >= array[cursorY].length) {
+            cursorX = 0;
+            cursorY++;
+        }
+        return element;
+    }
+}
