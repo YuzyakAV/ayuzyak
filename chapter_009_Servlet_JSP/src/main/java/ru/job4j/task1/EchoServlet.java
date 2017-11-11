ba81a9366b5484d161a3b0b61250bb8a7a3c3220
Андрей Юзяк <YuzyakAV@gmail.com>
2017-11-11 22:55:54 +0300

    add

---
+++ chapter_009_Servlet_JSP/src/main/java/ru/job4j/task1/EchoServlet.java
@@ -0,0 +1,38 @@
+package ru.job4j.task1;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+/**
+ * Мой первый сервлет.
+ */
+public class EchoServlet extends HttpServlet {
+    /**
+     * Логгер.
+     */
+    private static final Logger LOGGER = LoggerFactory.getLogger(EchoServlet.class);
+
+    /**
+     * Переопределенный метод doGet().
+     *
+     * @param req request.
+     * @param resp response.
+     * @throws ServletException
+     * @throws IOException
+     */
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.write("Hello World!!!");
+        out.write("<br>" + new Date());
+    }
+}
---
+++ chapter_009_Servlet_JSP/src/main/java/ru/job4j/task1/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * @author Ayuzyak
+ * @version 1.0
+ * @since 11.11.2017
+ */
+package ru.job4j.task1;
