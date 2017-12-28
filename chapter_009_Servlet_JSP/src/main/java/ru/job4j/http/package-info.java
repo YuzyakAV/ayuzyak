8f7ec96a92df99ff8846009a03e228f82153d78b
YuzyakAV <YuzyakAV@gmail.com>
2017-12-28 15:49:30 +0300

    add

--- chapter_009_Servlet_JSP/src/main/java/ru/job4j/http/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * @author Ayuzyak
- * @version 1.0
- * @since 15.11.2017
- */
-package ru.job4j.http;
--- chapter_009_Servlet_JSP/src/main/java/ru/job4j/task1/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * @author Ayuzyak
- * @version 1.0
- * @since 11.11.2017
- */
-package ru.job4j.task1;
